641bf15bdadd73d84811e6eab2fab3823458f326
akmalaf111 <akmalaf111@gmail.com>
2019-10-10 08:33:29 +0700

    first commit

---
+++ app/src/main/java/com/arrifqiaziz/tablayout/VpAdapter.java
@@ -0,0 +1,32 @@
+package com.arrifqiaziz.tablayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class VpAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> FragmentListTitles = new ArrayList<>();
+    public VpAdapter(FragmentManager fm) {
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+    @Override
+    public int getCount() {
+        return FragmentListTitles.size();
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return FragmentListTitles.get(position);
+    }
+    public void addFragment(Fragment fragment, String Title){
+        fragmentList.add(fragment);
+        FragmentListTitles.add(Title);
+    }
+}
